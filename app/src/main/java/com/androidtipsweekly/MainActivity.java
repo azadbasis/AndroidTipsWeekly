6df5048abd1029eba2e18113f0117237bfffc1da
Azhar <azadbasis@gmail.com>
2020-03-11 08:58:36 +0600

    JobScheduler

--- app/src/main/java/com/androidtipsweekly/MainActivity.java
+++ app/src/main/java/com/androidtipsweekly/MainActivity.java
@@ -1,8 +1,13 @@
 package com.androidtipsweekly;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        JobInfo jobInfo = new JobInfo.Builder(1001, new ComponentName(this, MyJobService.class))
+                .setMinimumLatency(0)
+                // .setPeriodic(2000)
+                .build();
+        jobScheduler.schedule(jobInfo);
+      //  scheduleJobFirebaseToRoomDataUpdate();
+    }
+    private void scheduleJobFirebaseToRoomDataUpdate(){
+        JobScheduler jobScheduler = (JobScheduler)getApplicationContext()
+                .getSystemService(JOB_SCHEDULER_SERVICE);
+
+        ComponentName componentName = new ComponentName(this,
+                MyJobService.class);
+
+        @SuppressLint("MissingPermission") JobInfo jobInfo = new JobInfo.Builder(1, componentName)
+                .setPeriodic(4000).setRequiredNetworkType(
+                        JobInfo.NETWORK_TYPE_NOT_ROAMING)
+                .setPersisted(true).build();
+        jobScheduler.schedule(jobInfo);
     }
 }
---
+++ app/src/main/java/com/androidtipsweekly/MyJobService.java
@@ -0,0 +1,26 @@
+package com.androidtipsweekly;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.util.Log;
+
+public class MyJobService extends JobService {
+
+    public static final String TAG="MyJobService";
+    public MyJobService() {
+    }
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        Log.i(TAG, "onStartJob: "+jobParameters.getJobId());
+        jobFinished(jobParameters,false);
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        Log.i(TAG, "onStopJob: "+jobParameters.getJobId());
+        return false;
+    }
+
+}
